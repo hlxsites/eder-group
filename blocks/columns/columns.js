183f2fab9b9768e9479b37c3a3ddda10689272cd
Mike Tilburg <mtilburg@adobe.com>
2023-01-11 17:32:36 +0100

    forgot to commit the lint error changes... here they are

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -2,18 +2,16 @@ export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
 
-  // If this is a 'card' on the page like 
+  // If this is a 'card' on the page like
   // https://www.eder-gmbh.de/unternehmen/geschaeftsbereiche/
-  if (block.classList.contains('row')){
-
+  if (block.classList.contains('row')) {
     const blockChildren = block.firstElementChild.children;
-    for (let i = 0; i < blockChildren.length; i++) {
-
+    for (let i = 0; i < blockChildren.length; i += 1) {
       // alternate backgroun color between cards
-      if ( i % 2 === 0) {
-        blockChildren[i].classList.add("gray-bg");
+      if (i % 2 === 0) {
+        blockChildren[i].classList.add('gray-bg');
       } else {
-        blockChildren[i].classList.add("white-bg")
+        blockChildren[i].classList.add('white-bg');
       }
 
       if (block.classList.contains('button-container')) {
@@ -22,17 +20,15 @@ export default function decorate(block) {
 
       // style each card with separators, and buttons styles based on background
       const children = blockChildren[i].children;
-      for (let j = 0; j < children.length; j++) {
-
+      for (let j = 0; j < children.length; j += 1) {
         // Add Style to Buttons
-        if (children[j].classList.contains('button-container')){
-
+        if (children[j].classList.contains('button-container')) {
           // update classes
-          children[j].children[0].classList.remove('primary')
+          children[j].children[0].classList.remove('primary');
           children[j].children[0].classList.add('coa-button', 'grey-coa-filled');
         }
 
-        if (j == 0 || j == 2) {
+        if (j === 0 || j === 2) {
           const $left = children[j];
           if ($left) {
             // Add separator
