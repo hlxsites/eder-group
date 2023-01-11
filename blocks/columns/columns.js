aefc2bef7efd84013457d5a9830ebed625a0ede9
Mike Tilburg <mtilburg@adobe.com>
2023-01-11 16:44:55 +0100

    improvement to buttons

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -3,29 +3,51 @@ export default function decorate(block) {
   block.classList.add(`columns-${cols.length}-cols`);
 
   //console.log (block.classList);
+  // If this is a 'card' on the page like 
+  // https://www.eder-gmbh.de/unternehmen/geschaeftsbereiche/
   if (block.classList.contains('row')){
 
     const blockChildren = block.firstElementChild.children;
     for (let i = 0; i < blockChildren.length; i++) {
 
-      // alternate backgroun color
+      // alternate backgroun color between cards
       if ( i % 2 === 0) {
-        blockChildren[i].classList.add("gray-bg")
+        blockChildren[i].classList.add("gray-bg");
+      } else {
+        blockChildren[i].classList.add("white-bg")
       }
 
+      if (block.classList.contains('button-container')) {
+        blockChildren[i].classList.add('grey-button-filled');
+      }
 
+      // style each card with separators, and buttons styles based on background
+      const children = blockChildren[i].children;
+      for (let j = 0; j < children.length; j++) {
 
-        // add separator
-        const children = blockChildren[i].children;
-        for (let j = 0; j < children.length; j += 2) {
-            if (j == 0 || j == 2) {
-              const $left = children[j];
-              if ($left) {
-                  const hr = document.createElement('hr');
-                  blockChildren[i].insertBefore(hr, $left.nextElementSibling);
-              }
-            }
+        //console.log(children[j])
+        // Combine buttons into a div and style
+        if (children[j].classList.contains('button-container')){
+          const div1 = document.createElement('div');
+          div1.classList.add('button-container');
+          //blockChildren[i].insertBefore(div1, children[j]);
+          //console.log("here")
+          //console.log(children[j].children[0])
+
+          // update classes
+          children[j].children[0].classList.remove('primary')
+          children[j].children[0].classList.add('coa-button', 'grey-coa-filled');
         }
+
+        if (j == 0 || j == 2) {
+          const $left = children[j];
+          if ($left) {
+            // Add separator
+            const hr = document.createElement('hr');
+            blockChildren[i].insertBefore(hr, $left.nextElementSibling);
+          }
+        }
+      }
     }
   }
 }
